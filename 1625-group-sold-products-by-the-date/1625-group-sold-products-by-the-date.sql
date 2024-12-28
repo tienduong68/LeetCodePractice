2b3ac9e7e0520603716c4b518592325e7c92870c
tienduong68 <tienduongfake123@gmail.com>
2024-12-28 15:00:29 +0700

    Time: 909 ms (11.47%), Space: 0B (100%) - LeetHub

---
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold, GROUP_CONCAT(DISTINCT(product) SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
+
